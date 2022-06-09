fbee27af7355ffa3b03550280133776e02e3c958
simon <simon.mandereau@gmail.com>
2022-06-09 13:57:15 +0200

    boucle pour faire appel à 1 élément à la fois

--- P5-Dev-Web-Kanap-master/front/html/index.js
+++ P5-Dev-Web-Kanap-master/front/html/index.js
@@ -1,13 +1,14 @@
-/*Search all items in the API and get promise*/
-fetch("http://localhost:3000/api/products")
-    .then(function(items) {
-        if (items.ok) {
-            return items.json();
-        }
-    })
-    .then(function(value){
-        console.log(value)
-    })
-    .catch(function(err){
-    });
+let url ="http://localhost:3000/api/products";
 
+/*Search all items in the API and get promise*/
+fetch(url)
+    .then((response) => 
+    response.json().then((data) => {
+        let lenghtArray = 8;
+        for (let eltData of data){
+            if ( i = 0, i<lenghtArray, i++);
+            console.log(eltData)
+        };
+    })
+    .catch((error) => console.log("Pas de réponse de l'API"))
+);
