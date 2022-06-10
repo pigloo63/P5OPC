f5a629d9595a47e5aaed6d1337b2b5d6ad629c91
simon <simon.mandereau@gmail.com>
2022-06-10 09:36:05 +0200

    insertion des différents canapé (img, description, etc....)

--- P5-Dev-Web-Kanap-master/front/html/index.js
+++ P5-Dev-Web-Kanap-master/front/html/index.js
@@ -1,14 +1,24 @@
 let url ="http://localhost:3000/api/products";
 
-/*Search all items in the API and get promise*/
 fetch(url)
     .then((response) => 
-    response.json().then((data) => {
-        let lenghtArray = 8;
-        for (let eltData of data){
-            if ( i = 0, i<lenghtArray, i++);
-            console.log(eltData)
-        };
-    })
+        response.json()
+            .then((data) => { /*Search all items in the API and get promise*/
+                for (let eltData of data){ 
+                    var part = document.getElementById("items");
+                    var templt = part.children[0].cloneNode(true);
+                    
+                    /*Inserting api values*/
+                    templt.href = './product.html?id=' + eltData._id;
+                    templt.getElementsByTagName('img')[0].src = eltData.imageUrl;
+                    templt.getElementsByClassName('productName')[0].textContent = eltData.name;
+                    templt.getElementsByClassName('productDescription')[0].textContent = eltData.description;
+
+                    part.appendChild(templt);
+                };
+                part.children[0].remove();
+            })
     .catch((error) => console.log("Pas de réponse de l'API"))
 );
+
+
